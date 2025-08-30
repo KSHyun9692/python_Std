8ff807ba8ca7092727d9c7f9f45a07cf19e4edb1
KSHyun9692 <KSHyun9692@gmail.com>
2025-08-30 23:21:20 +0900

    variable

---
+++ 04.변수/01var.py
@@ -0,0 +1,22 @@
+a = 2
+b = 3
+c = a + b
+print(c)
+
+#string
+name = "김승현"
+print(name)
+
+#int
+age = 30
+print(30)
+
+#boolean
+boolean_test = True
+print(boolean_test)
+boolean_test2 = False
+print(boolean_test2)
+
+#type check
+print(type(c))
+
