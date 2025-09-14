cfd350a47f870e8e4cb0f17ef50107a03c8fc0c4
KSHyun9692 <KSHyun9692@gmail.com>
2025-09-14 21:46:20 +0900

    data

---
+++ 07.method/list.py
@@ -0,0 +1,14 @@
+week = ["mon", "tue", "wen", "thu", "fri"]
+print(week)
+print(week[2])
+print(week.count("mon"))
+""" print(week.clear()) """
+
+week.append("sat")
+week.append("sun")
+
+print(week)
+
+
+name = "ksh"
+print(name.upper())
