d8098de7e32e8c833abb5633757400b343e67b80
KSHyun9692 <KSHyun9692@gmail.com>
2025-10-23 20:27:08 +0900

    기초 변수

--- 04.변수/01var.py
+++ 04.변수/01var.py
@@ -20,3 +20,28 @@ print(boolean_test2)
 #type check
 print(type(c))
 
+
+string = "입력한 문자는 : "
+x = input("입력하세요.")
+print(string + x)
+
+
+price = input("가격을 입력하세요 : ")
+print("제품의 가격 : ", price)
+
+
+name = "유미"
+level = 6
+hp = 950
+string = "나랑 유미랑!"
+
+print(name, level,  hp,  string)
+print(name +  str(level) +  str(hp) +  string)
+
+name = input("포켓몬 이름을 입력하세요")
+print("입력하신 이름은 : ", name)
+
+
+name = input("포켓몬 이름을 입력하세요 : ")
+level = input("포켓몬 레벨을 입력하세요 : ")
+print("입력하신 이름은 : ", name, "레벨은 : ", level)
